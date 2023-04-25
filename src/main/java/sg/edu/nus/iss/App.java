f21f76acdd66eb53234933732a21b3bfdc1d0186
russell258 <duckie258@gmail.com>
2023-04-25 13:32:42 +0800

    input reader and list condition pushed

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -1,13 +1,53 @@
 package sg.edu.nus.iss;
 
-/**
- * Hello world!
- *
- */
+import java.util.ArrayList;
+// import java.io.Console;
+import java.util.Scanner;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Welcome to your shopping cart");
+        //Console con = System.console();
+        Scanner scan = new Scanner(System.in);
+
+        String commandInput = " ";
+        String secondInput = " ";
+        ArrayList<String> cartItems = new ArrayList<>();
+
+
+        //loop to keep asking for input
+        while(!commandInput.equals("quit")){
+            //use scanner to ask for input and check first command
+    
+            commandInput = scan.next();
+
+            // cases for list
+            if (commandInput.equals("list")){
+                if (cartItems.size()>0){
+                    for (String item: cartItems){
+                        int i = 0;
+                        System.out.println(i + ". " + item);
+                    }
+                }else{
+                    System.out.println("Your cart is empty");
+                }
+            }
+
+
+                    
+
+        // case for add
+
+                // secondInput = commandInput.substring(5);
+                // System.out.println(secondInput);
+
+
+        //case for delete
+
+        }
+
+        scan.close();
     }
 }
