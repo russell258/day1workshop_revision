c01ee12bc1f4915f35a7e3fe033aeda01e089322
russell258 <duckie258@gmail.com>
2023-04-30 03:20:48 +0800

    users listing in class done

--- src/main/java/sg/edu/nus/iss/ShoppingCartDB.java
+++ src/main/java/sg/edu/nus/iss/ShoppingCartDB.java
@@ -1,11 +1,17 @@
 package sg.edu.nus.iss;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ShoppingCartDB {
 
+    List<String> itemsToSave = new ArrayList<String>();
+    String[] itemToSave;
+
     //previous shoppingCart functions
 
-    public void list(){
+    public void list(ArrayList<String>cartItems){
         
     }
 
@@ -21,16 +27,18 @@ public class ShoppingCartDB {
 
     //new methods for reading, writing, 
 
-    public void save(){
-
+    public void save(String[] itemsToSave, String savedUser){
+        this.itemsToSave = itemsToSave;
     }
 
     public void login(){
 
     }
 
-    public void users(){
-
+    public static void users(File directory){
+        for (String user: directory.list()){
+            System.out.println(user.toString());
+        }
     }
 
 }
