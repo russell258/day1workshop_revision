53f97d0d12418ca83a4bac9dee6a36bf8b638863
russell258 <duckie258@gmail.com>
2023-05-09 15:26:00 +0800

    day 1 ws revision

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -1,82 +1,89 @@
 package sg.edu.nus.iss;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App 
 {
-    public static void main( String[] args ) throws IOException
-    {
-        // task 1: check if directory for shoppingcart, cartdb that holds all the users exist. else create it
-        // if no args specified, use default directory db
-        String dirName ="";
+    public static void main( String[] args ){
 
-        if (args.length>0){
-            dirName = args[0];
-            }else{
-                dirName = "cartDB";
-                System.out.println("You have not passed in any argument so the default directory cartDB will be used");
-            }
-        File newDirectory = new File(dirName);
-        if (!newDirectory.isDirectory()){
-            newDirectory.mkdir();
-        }
-        System.out.println("Welcome to your shopping cart");
-        //use scanner to ask for input and check first command
-        //loop to keep asking for input
+
+        //print out welcome to your shopping cart
+        System.out.println("Welcome to your shopping cart!");
+
+        //declare scanner for input or can use bufferedReader too
         Scanner scan = new Scanner(System.in);
-        ArrayList<String> cartItems = new ArrayList<>();
-        String commandInput = "";
+        String input = "";
         String secondInput = "";
-        String dirPath = "";
-        Integer deleteNumber = 0;
-        boolean isLoggedIn = false;
+        int secondInt = 0;
+        String[] addList = null;
+        
 
-        while(!commandInput.equals("quit")){
-        // call list method return list of files in the directory
-            commandInput = scan.next().trim();
-            if (commandInput.equals("users")){
-                ShoppingCartDB.users(newDirectory);
-            }
+        //declare arraylist shoppingcart for storing
+        List <String> shoppingCart = new ArrayList<String>();
 
-        // call login method
-            if (commandInput.equals("login")){
-                secondInput = scan.nextLine().trim();
-                dirPath = newDirectory + File.separator + secondInput;
-                ShoppingCartDB.login(dirPath, secondInput, cartItems);
-                isLoggedIn = true;
-            }
-            
-        // call add method
-            if (commandInput.equals("list")){
-                ShoppingCartDB.list(cartItems);
-            }
-
-            if (commandInput.equals("add")){
-                secondInput = scan.nextLine().trim();
-                if (!secondInput.equals("")){
-                    secondInput = secondInput.trim().replaceAll(" +", " ");
-                    ShoppingCartDB.add(secondInput, cartItems);
-                }
-            }
-
-            if (commandInput.equals("delete")){
-                secondInput=scan.nextLine().trim();
-                if (secondInput.matches(".*\\d+.*")){
-                    deleteNumber = Integer.parseInt(secondInput);
-                    ShoppingCartDB.delete(cartItems, deleteNumber);
-                }
-            }
-            //save, write from current arraylist into the user file and flush
-            if (commandInput.equals("save")){
-                if (isLoggedIn){
-                    ShoppingCartDB.save(dirPath, cartItems);
+        //important to use .equals instead of != for string
+        while (!input.equals("quit")){
+            //put the input inside declare scan so no infinite loop and have scan input.
+            // print ">" before scan
+            System.out.print("> ");
+            input = scan.next();
+            //case for list
+            if (input.equals("list")){
+                if (shoppingCart.size()!=0){
+                    int i=1;
+                    for (String item: shoppingCart){
+                        System.out.println(i + ". "+item);
+                        i++;
+                    }
                 }else{
-                    System.out.println("Please log in first");
+                    System.out.println("Your cart is empty");
                 }
             }
+            // case for add
+            // split the multiple add inputs
+            // remove the whitespace and commas
+            // print out added or already have
+            if (input.equals("add")){
+                secondInput = scan.nextLine().trim().toLowerCase();
+                if (!secondInput.isEmpty()){
+                    //beautiful regex https://stackoverflow.com/questions/49089755/how-to-split-expression-by-comma-and-space-in-java
+                    addList = secondInput.split("[ ,]+");
+
+                    for (String addItem: addList){
+                        if (shoppingCart.contains(addItem)){
+                            System.out.println("You have "+ addItem + " in your cart");
+                        }else{
+                            shoppingCart.add(addItem);
+                            System.out.println(addItem + " added to your cart");
+                        }
+
+
+                    }
+                }else{
+                    System.out.println("Please add something");
+                }
+            }
+
+            //case for delete
+            if (input.equals("delete")){
+                secondInput = scan.nextLine();
+                if (secondInput.isEmpty() || !secondInput.matches(".*\\d+.*")){
+                    System.out.println("Incorrect input");
+                }else{
+                    secondInt = Integer.parseInt(secondInput.trim()) - 1;
+                    if (secondInt >= shoppingCart.size()){
+                        System.out.println("Incorrect item index");
+                    }else{
+                        System.out.println(shoppingCart.get(secondInt) + " removed from cart");
+                        shoppingCart.remove(secondInt);
+                    }
+                }
+
+
+            }
+
         }
         scan.close();
     }
